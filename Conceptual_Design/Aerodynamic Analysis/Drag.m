e4147d12f84f2f4b81d78852515d8b4192984856
iecoo <92816799+iecoo@users.noreply.github.com>
2021-11-11 10:55:37 +0000

    aerodynamic analysis script updates

--- Conceptual_Design/Aerodynamic Analysis/Drag.m
+++ Conceptual_Design/Aerodynamic Analysis/Drag.m
@@ -4,7 +4,18 @@
 %AVD
 
 %% Drag Analysis
-%Parasite drag & lift-induced
+%Parasite drag (zero-lift) & lift-induced
+
+%% Inputs from other scripts
+S_ratio=6; %=S_wet/S_ref
+
 
 %% M_dd
 %Drag divergence Mach number
+
+%% Parasite Drag
+
+% C_D_0 initial estimate (not very accurate) - light aircaraft, twin engine
+aero_analysis.drag.C_d0_estimate=0.0045*S_ratio;
+
+%% Parasite Drag: Component buildup
