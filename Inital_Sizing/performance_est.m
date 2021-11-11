7e493099aabc7b7cf45fb4e37c84f4bdf6ca4d94
cking <chorleyblueking@gmail.com>
2021-11-11 15:53:23 +0000

    Update performance_est.m

--- Inital_Sizing/performance_est.m
+++ Inital_Sizing/performance_est.m
@@ -19,4 +19,9 @@ perf.Sr = 3 * V_TO;
 
 R = (1.15 * sizing.takeoff.v_stall)^2 / (0.2 * 9.81); %V_TR = 1.15*stall speed
 perf.Str = sqrt(R^2 - (R - H_obs)^2); 
+
+sizing.takeoff.cd = sizing.takeoff.cd_0 + (sizing.takeoff.cl_max^2) /  (pi * sizing.AR * sizing.takeoff.e); 
+LoverD_TR = sizing.takeoff.cl_max / sizing.takeoff.cd; 
+gamma_climb = asin(T / sizing.W0 - 1 / LoverD_TR);
+H_TR = R * (1 - cos(gamma_climb)); 
 %%
