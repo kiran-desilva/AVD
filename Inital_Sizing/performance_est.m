bffc7a0e386d0cbd7df73ee013783c59c8e48bb5
cking <chorleyblueking@gmail.com>
2021-11-11 16:21:47 +0000

    Update performance_est.m

--- Inital_Sizing/performance_est.m
+++ Inital_Sizing/performance_est.m
@@ -13,7 +13,7 @@ rolltime = 3; %raymer
 
 perf.Ka = (1.225 / (2 * sizing.W0 / design_S_ref)) * (mu * sizing.takeoff.cl_max - sizing.takeoff.cd_0 - (sizing.takeoff.cl_max^2) / (pi * sizing.AR *sizing.takeoff.e));
 perf.Kt = design_t_w - mu;
-perf.Sg = (1 / (2 * 9.81 * perf.Ka)) * log(abs((perf.Kt + perf.Ka * V_TO^2) / (perf.Kt + perf.Ka * Vinit^2));
+perf.Sg = (1 / (2 * 9.81 * perf.Ka)) * log(abs((perf.Kt + perf.Ka * V_TO^2) / (perf.Kt + perf.Ka * Vinit^2)));
 
 perf.Sr = 3 * V_TO; 
 
