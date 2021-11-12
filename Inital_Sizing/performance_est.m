576d8975fe0b5920f0fdf45b810eea6f4495084f
cking <chorleyblueking@gmail.com>
2021-11-12 13:58:00 +0000

    Update performance_est.m

--- Inital_Sizing/performance_est.m
+++ Inital_Sizing/performance_est.m
@@ -29,10 +29,10 @@ perf.Sto = 1.15 * (perf.Sg + perf.Sr + perf.Str); %FAR25 15% safety factor all e
 gamma_climb_1eng =  asin(0.5 * design_t_w - 1 / LoverD_TR); %one engine operative
 gamma_min = 0.024; %FAR25 minimum climb for 2-engine a/c
 G = gamma_climb_1eng - gamma_min;
-%CL_climb = 1.2 * 
+CL_climb = 0.694 * sizing.takeoff.cl_max; %Gudmundsson
 T_av = 0.75 * 0.5 * design_t_w * sizing.W0 * ((5 + parameters.BPR) / (4 + parameters.BPR)); %one engine operative
 U = 0.01 * sizing.cl_max + 0.02; 
-%perf.BFL = (0.863 / (1 + 2.3 * G)) * ((design_w_s / (1.225 * 9.81 * CL_climb)) + H_obs) * (1 / (T_av / sizing.W0 - U) + 2.7) + 655;
+perf.BFL = (0.863 / (1 + 2.3 * G)) * ((design_w_s / (1.225 * 9.81 * CL_climb)) + H_obs) * (1 / (T_av / sizing.W0 - U) + 2.7) + 655;
 
 %% Landing 
 
