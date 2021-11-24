cbaa96748cf4ff96554ee2545e5b0f869b55312c
cking <chorleyblueking@gmail.com>
2021-11-24 20:49:06 +0000

    Update control_surface_sizing.m

--- Inital_Sizing/control_surface_sizing.m
+++ Inital_Sizing/control_surface_sizing.m
@@ -4,5 +4,9 @@ clc
 load("tailpane.mat");
 
 tailplaneC_elevatoredge = tailplane.Croot + 0.9 * (tailplane.Ctip - tailplane.Croot);
-control_surface.elevator_area = 0.5 * 0.32 * (tailplane.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.b;
+control_surface.elevator_area = 0.32 * (tailplane.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.b; %note 2 elevators
+
+
+rudderC_rudderedge = tailplane.Croot + 0.9 * (tailplane.Ctip - tailplane.Croot);
+control_surface.elevator_area = 0.32 * (tailplane.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.b; %note 2 elevators
 
