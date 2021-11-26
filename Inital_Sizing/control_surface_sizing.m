47941def9a50611c36e48818e5b3e6743e4b87d2
cking <chorleyblueking@gmail.com>
2021-11-26 17:27:08 +0000

    Update control_surface_sizing.m

--- Inital_Sizing/control_surface_sizing.m
+++ Inital_Sizing/control_surface_sizing.m
@@ -2,12 +2,18 @@ clear
 clc
 
 load("tailplane.mat");
+load("wing.mat");
 
 tailplaneC_elevatoredge = tailplane.initial.horizontal.Croot + 0.9 * (tailplane.initial.horizontal.Ctip - tailplane.initial.horizontal.Croot);
 control_surface.elevator_area = 0.32 * (tailplane.initial.horizontal.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.initial.horizontal.b; %note 2 elevators
 
 rudderC_rudderedge = tailplane.initial.vertical.Croot + 0.9 * (tailplane.initial.vertical.Ctip - tailplane.initial.vertical.Croot);
-control_surface.rudder_area =  0.5 * 3 * (tailplane.initial.vertical.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.initial.vertical.b; 
+control_surface.rudder_area =  0.5 * 0.3 * (tailplane.initial.vertical.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.initial.vertical.b; 
 
-aileronC_inner = tailplane.initial.vertical.Croot + 0.9 * (tailplane.initial.vertical.Ctip - tailplane.initial.vertical.Croot);
-control_surface.rudder_area =  0.5 * 3 * (tailplane.initial.vertical.Croot  + tailplaneC_elevatoredge) * 0.9 * 0.5 * tailplane.initial.vertical.b; 
+aileronC_inneredge = wing.Croot + 0.7 * (wing.Ctip - wing.Croot);
+aileronC_outeredge = wing.Croot + 0.95 * (wing.Ctip - wing.Croot);
+control_surface.aileron_area =  (1 - wing.HDL_PERC) * (aileronC_inneredge  + aileronC_outeredge) * 0.25 * 0.5 * wing.b; 
+
+flapC_inneredge = wing.Croot + 0.7 * (wing.Ctip - wing.Croot);
+flapC_outeredge = wing.Croot + 0.7 * (wing.Ctip - wing.Croot);
+control_surface.flap_area =  (1 - wing.HDL_PERC) * (flapC_inneredge  + flapC_outeredge) * 0. * 0.5 * wing.b; 
