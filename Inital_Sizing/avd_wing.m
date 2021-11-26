92790ce1b43c4df1c8d3add3a12b5be745ca0810
Nadscodeqween <92813140+Nadscodeqween@users.noreply.github.com>
2021-11-26 19:20:06 +0000

    hvgh

--- Inital_Sizing/avd_wing.m
+++ Inital_Sizing/avd_wing.m
@@ -35,9 +35,10 @@ CL_design_cruise=( (W_cruise_W_total)*(W_S) )/ (0.5*rho_cruise*V_cruise^2)
 
 %mach
 sweep_25=18.1692
-M_DD=0.7438
+M_DD=0.75
 M_DD_Eff=M_DD*sqrt(cosd(sweep_25))
 AR=7.8
+
 x=cosd(22)
 
 Cl_design_a=(CL_design_cruise/(0.9*0.95))/cosd(sweep_25)
@@ -48,24 +49,30 @@ Km=1.05
 %lambda_opt=0.45*(0.85)^(-0.036*sweep_angle)
 lambda_opt=0.45*exp(-0.036*sweep_25)
 lambda_min=0.2*(AR)^(0.25)*(cosd(sweep_25)^2)
-SP=(Max_takeoff_weight*9.8)/(W_S) 
+SP=(Max_takeoff_weight)/(W_S) 
 mainwing.b = sqrt(AR*SP) 
-
-lambda=lambda_min
+if lambda_min>lambda_opt
+    lambda=lambda_min
+else
+    lambda=lambda_op
+end
+mainwing.lambda=lambda
 
 mainwing.Croot= (2*SP)/(mainwing.b*(1+lambda));
 mainwing.Ctip=lambda*mainwing.Croot;
 C_mean=(2/3) * mainwing.Croot* ((1+lambda+lambda^2)/(1+lambda))
-
+mainwing.C_mean=C_mean
 %c_dist
 m=(0-mainwing.b/2)/(mainwing.Croot/mainwing.Ctip)
 
 y_bar=(mainwing.b/6)*((1+2*lambda)/(1+lambda))
-
+mainwing.y_bar=y_bar
 t_c= 0.3*cosd(sweep_25)*((1-((5+M_DD_Eff^2)/(5+(Km-0.25*Cl_design_a)^2))^3.5)*((sqrt(1-M_DD_Eff^2)/M_DD_Eff^2)))^(2/3)
 
 Re= (rho_cruise * V_cruise * C_mean )/ mu_cruise
+mainwing.Re=Re
 twist=-3
+mainwing.twist=twist
 
 [mainwing.sweepLE] = sweep_angle(sweep_25,0,25,AR,lambda);
 [mainwing.sweepTE] = sweep_angle(sweep_25,100,25,AR,lambda);
@@ -74,6 +81,8 @@ twist=-3
 % i_w= (y_bar/(b/2))*twist*(x_position) + i_w_mac
 alpha_0=-3
 CL_cruise=0.4809
+mainwing.alpha_0=alpha_0
+mainwing.CL_cruise=CL_cruise
 
 Cl_alpha=(1.6-0.4)/((16*(pi/180)--0*(pi/180))) %issue find clalpha
 i_w= (CL_cruise/Cl_alpha)+alpha_0 +0.4*twist
@@ -113,6 +122,7 @@ mainwing.HDL_Ctip=(1-mainwing.HDL_PERC)*mainwing.Ctip;
 
 
 sref=((mainwing.Croot+mainwing.Ctip)/2)*mainwing.b;
+mainwing.sref=sref
 %aileron starts at 70 of span
 mainwing.aileron_start_top=0.7*mainwing.b/2;
 mainwing.aileron_ypos_top=tand(mainwing.sweepHDL)*mainwing.aileron_start_top;
@@ -125,7 +135,7 @@ mainwing.HDL_start=1.52/2 + 0.05*mainwing.b %therefore 0.05 not including fuesel
 C_HDL_start_root=((2*(mainwing.Ctip-mainwing.Croot))/mainwing.b)*(mainwing.HDL_start) +mainwing.Croot
 C_70=((2*(mainwing.Ctip-mainwing.Croot))/mainwing.b)*(0.7*mainwing.b*0.5) +mainwing.Croot
 sflapped=(((C_HDL_start_root+C_70)/2)*(0.7*mainwing.b-mainwing.HDL_start))
-
+mainwing.sflapped=sflapped
 mainwing.C_HDL_root=(1-mainwing.HDL_PERC)*(((2*(mainwing.Ctip-mainwing.Croot))/mainwing.b)*mainwing.HDL_start +mainwing.Croot);
 
 
@@ -133,6 +143,7 @@ mainwing.C_HDL_root=(1-mainwing.HDL_PERC)*(((2*(mainwing.Ctip-mainwing.Croot))/m
 x_dist=[0:mainwing.b/2]
 chord_dist=((2*(mainwing.Ctip-mainwing.Croot))/mainwing.b)*x_dist +mainwing.Croot
 c_mac=((2*(mainwing.Ctip-mainwing.Croot))/mainwing.b)*y_bar +mainwing.Croot
+mainwing.c_mac=c_mac
 delta_CL=q-CLmax_eff
 
 syms x sweep_hdl
@@ -156,10 +167,10 @@ plot([mainwing.sweepHDL, mainwing.sweepHDL],[1,2])
 k=(delta_CL*sref) / (0.9*HDL_coeff*sflapped*cosd(mainwing.sweepTE))
 %k=cbar/c
 cbar_root=k*mainwing.Croot
-cf=cbar_root-mainwing.Croot
-f=cf/mainwing.Croot
-perc_chord=(mainwing.Croot-cf)/mainwing.Croot
-
+HDL_chord=cbar_root-mainwing.Croot
+fraction_HDLchord_over_wingchord=HDL_chord/mainwing.Croot
+perc_chord=(mainwing.Croot-HDL_chord)/mainwing.Croot
+mainwing.fraction_HDLchord_over_wingchord=fraction_HDLchord_over_wingchord
 
 [fig] = hdl(mainwing)
 
@@ -167,6 +178,11 @@ perc_chord=(mainwing.Croot-cf)/mainwing.Croot
 %s_wet=s_exposed*(1.977+0.52(t_c))
 
 
-
+mainwing.AR=AR
+mainwing.sweep_25=sweep_25
+mainwing.M_DD=M_DD
+mainwing.M_DD_Eff=M_DD_Eff
+mainwing.Cl_design_a=Cl_design_a
+mainwing.Km=Km
 
 % save('wing','wing.mat')
