6892557bcab4d6cf97d32c88981927798fdb948e
iecoo <92816799+iecoo@users.noreply.github.com>
2021-12-02 16:36:56 +0000

    transition

--- Inital_Sizing/aero_ana_summary.m
+++ Inital_Sizing/aero_ana_summary.m
@@ -2,6 +2,7 @@
 %  Summary script (with plots)
 clear
 clc
+close all
 
 load 'aero_analysis.mat'
 load 'tailplane.mat'
@@ -56,6 +57,32 @@ aero_analysis.summary.L_D_loiter=aero_analysis.summary.cl_loiter/aero_analysis.d
 aero_analysis.summary.e_aircraft=aero_analysis.induced_drag.e_theoretical(1)+aero_analysis.induced_drag.e_theoretical(2)*tailplane.horizontal.s/wing.Sref;
 aero_analysis.summary.e_v2=aero_analysis.induced_drag.wing.e(1)+aero_analysis.induced_drag.tail.e(1)*tailplane.horizontal.s/wing.Sref;
 
+%% Drag polar
+
+induced_AR=[wing.Ar, tailplane.horizontal.Ar];
+cl_wing_polar=[0:0.001:2];
+cl_tail_polar=[0:0.001:2];
+e_polar=aero_analysis.induced_drag.e_theoretical;
+
+cl_total_polar=cl_wing_polar+cl_tail_polar*tailplane.horizontal.s/wing.Sref;
+
+cd_wing_polar=(cl_wing_polar).^2/(pi*induced_AR(1)*e_polar(1));
+cd_tail_polar=(cl_tail_polar).^2/(pi*induced_AR(2)*e_polar(2))*1*tailplane.horizontal.s/wing.Sref;
+cd_total_polar=aero_analysis.drag.cd0(1)+cd_wing_polar+cd_tail_polar+aero_analysis.drag.wave(1);
+
+%l_d_cruise=aero_analysis.summary.L_D_cruise.*cl_wing_polar;
+l_d_cruise=13.2*cl_wing_polar;
+
+figure
+plot(cd_total_polar,cl_total_polar)
+hold on
+plot(cl_wing_polar,l_d_cruise)
+plot(aero_analysis.drag.cd_total(1),aero_analysis.summary.cl_cruise, 'o') 
+hold off
+grid on
+grid minor
+xlim([0 0.15])
+
 %% Plots
 figure
 x=x*180/pi;
@@ -70,6 +97,10 @@ plot(x+zero_AoA+aero_analysis.wing.HLD.delta_alpha(2),cl_alpha_wing_approach)
 plot(x+zero_AoA,cl_alpha_wing_loiter_clean)
 legend('Cl_max clean', 'Cl_max_approach', 'Cl_max_TO', 'Clean Cruise', 'Max Cruise', 'Takeoff', 'Approach', 'Loiter')
 
+aero_analysis.summary.zero_aoa.TO=zero_AoA+aero_analysis.wing.HLD.delta_alpha(1);
+aero_analysis.summary.zero_aoa.landing=zero_AoA+aero_analysis.wing.HLD.delta_alpha(2);
+
+aero_analysis.summary.cl_transition=0.9*aero_analysis.summary.cl_max_TO;
 ylim ([0 3])
 xlim([-15 25])
 title 'Wing'
