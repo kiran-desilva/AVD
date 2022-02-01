74bc7f945fdedc75105e070d635dab9e77962c38
iecoo <92816799+iecoo@users.noreply.github.com>
2022-02-01 13:02:10 +0000

    avl scripts

--- PreliminaryAirframe/aero_forces.m
+++ PreliminaryAirframe/aero_forces.m
@@ -5,5 +5,17 @@
 % - calculate shear force distribution
 % - calculate bending moment distribution
 
+
+%% Housekeeping
+clear
+clc
+
 %% Extract data
 
+a=importfile("C:\Users\izzye\OneDrive\Documents\GitHub\AVD\PreliminaryAirframe\AVL Data Files\AVL_1.dat", [21, 84])
+
+j=a(:,1);
+y_le=a(:,4);
+c_cl=a(:,5);
+chord=a(:,7);
+area=a(:,10);
