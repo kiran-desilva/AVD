2d7f5ccd21bdbfc33d63e06adaf6c3488266b905
iecoo <92816799+iecoo@users.noreply.github.com>
2022-02-01 12:25:21 +0000

    AVL

---
+++ PreliminaryAirframe/aero_forces.m
@@ -0,0 +1,9 @@
+%% AVD Script
+% steps:
+% - extract data from .txt file
+% - calculate Lift distribution
+% - calculate shear force distribution
+% - calculate bending moment distribution
+
+%% Extract data
+
