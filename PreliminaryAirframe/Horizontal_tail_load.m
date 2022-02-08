1b1462af46d19ca58f162e939c52d98c150b4615
cking <chorleyblueking@gmail.com>
2022-02-08 12:09:59 +0000

    dive tail loading

--- PreliminaryAirframe/Horizontal_tail_load.m
+++ PreliminaryAirframe/Horizontal_tail_load.m
@@ -1,21 +1,33 @@
 clear
 clc
 
-%loading on horizontal tail for unit lift force
+%loading on horizontal tail as a factor of unit lift force
 
 s_h = 2.3887; %m
 U = 100;
-L = 1;
 AR_h = 3.9;
 rho = 1.225; 
 Sref_h = 1.9283; 
+Sref_w = 10.32;
+
+X_cg = 14.94 * 0.3048; %ft to m ref point nose
+X_acHtail = 10.3474; 
+X_acWing = 4.3620
+ULF = ? % ultimate load factor
+W0 = 3128.2 * 9.81; %takeoff weight
+Lwing = ULF * W0;
+Vdive = ?;
+Cm0 = -0.0266;
+M0w = 0.5 * rho * Vdive^2 * Sref_w * Cm0;
+
+L_Htail = (Lwing * (X_cg - XacWing) + M0w) / (X_acHtail - X_cg); 
 
 y = linspace(-s_h/2,s_h/2,100);
 gamma0 = (8 * s_h * 1) / (pi * AR_h * rho * U * Sref_h); %L = 1
 gamma = @(y) gamma0 * sqrt(1 - (y ./ (s_h/2)).^2); 
 TailLoadinit = rho * U .* gamma(y); 
 TailLoadintegral = trapz(y,TailLoadinit); 
-TailLoad = @(y) L * rho * U * gamma(y) / TailLoadintegral; %unit overall tail lift 
+TailLoad = @(y) L_Htail * rho * U * gamma(y) / TailLoadintegral; %unit overall tail lift 
 
 figure
 plot(y, TailLoad(y))
