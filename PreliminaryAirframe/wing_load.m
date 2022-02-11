da7c8f2597b35768d169d97677d15859eb6cbe86
Pavol Margitfalvi <margitpav1@gmail.com>
2022-02-11 21:21:58 +0000

    Wing loadcases (hopefully) done

--- PreliminaryAirframe/wing_load.m
+++ PreliminaryAirframe/wing_load.m
@@ -6,17 +6,15 @@
 %	cl_dist					-	array representing discretised sectional lift coefficient distribution
 %	spanwise_disc			-	array with spanwise points at which cl is sampled
 %								THIS HAS TO BE UNIFORMLY SPACED!!!
-% function [shear_dist, bm_dist, torsion_dist] = wing_load(n, Vinf_eas, wing_fuel_weight_kg, cl_dist, spanwise_disc, isLanding)
-	clear
-	clc
-	close all;
+function [shear_dist, bm_dist, torque_dist] = wing_load(n, Vinf_eas, wing_fuel_weight_kg, cl_dist, spanwise_disc, cm0, isLanding, doPlot, loadingCaseStr)
+	load('loadcase.mat')
 
-	n = 1;
-	Vinf_eas = 100;
-	wing_fuel_weight_kg = 100;
-	cl_dist = ones(1, 1000);
-	spanwise_disc = linspace(0, 8.94/2, 1000);
-	isLanding = false;
+	% n = 1;
+	% Vinf_eas = 100;
+	% wing_fuel_weight_kg = 100;
+	% cl_dist = ones(1, 1000);
+	% spanwise_disc = linspace(0, 8.94/2, 1000);
+	% isLanding = false;
 
 	delta_s = spanwise_disc(2) - spanwise_disc(1);
 	g = 9.81;
@@ -80,18 +78,19 @@
 	top_linspace = linspace(top_edge_intercept, semispan, 100);
 	bottom_linspace = linspace(bottom_edge_intercept, semispan, 100);
 
-	hold on;
-	plot(top_linspace, top_edge(top_linspace), color);
-	plot(bottom_linspace, bottom_edge(bottom_linspace), color);
-	plot([semispan semispan], [bottom_edge(semispan) top_edge(semispan)], color)
-	hold off
+	% hold on;
+	% plot(top_linspace, top_edge(top_linspace), color);
+	% plot(bottom_linspace, bottom_edge(bottom_linspace), color);
+	% plot([semispan semispan], [bottom_edge(semispan) top_edge(semispan)], color)
+	% hold off
 
 	%% Wing aero loading
-	lift.gamma0 = 4*semispan*Vinf_eas*1.3/(pi*7.8);
-	lift.gamma(x) = gamma0*sqrt(1 - (x/semispan)^2);
-	lift.L(x) = 1.225*Vinf_eas*gamma(x);
-	lift.L_dist = L(spanwise_disc);
-	% L_dist = 0.5*n*rho0*Vinf_eas^2*c(spanwise_disc).*cl_dist;
+	% lift.gamma0 = 4*semispan*Vinf_eas*1.3/(pi*7.8);
+	% lift.gamma(x) = lift.gamma0*sqrt(1 - (x/semispan).^2)
+	% lift.L(x) = rho0*Vinf_eas*lift.gamma(x)
+	% lift.L_dist = double(lift.L(spanwise_disc))
+	lift.L_dist = 0.5*2.5*rho0*Vinf_eas^2*c(spanwise_disc).*cl_dist;
+	double(trapz(spanwise_disc, lift.L_dist))
 
 	%% Wing weight loading
 	% M(x1, x2) = M_wing*V(x1, x2)/V_wing
@@ -121,8 +120,6 @@
 	fuel.percent_full = wing_fuel_weight_kg/fuel.full_wing_mass;
 	fuel.loading(x1, x2) = piecewise((x1 >= bottom_edge_intercept) & (x2 <= 0.95*semispan), -g*n*fuel.density*fuel.percent_full*V(x1, x2)/delta_s, 0);
 
-	combined_loading = double(uc.loading(spanwise_disc) + fuel.loading(x1_arr, x2_arr) + lift.L_dist + wing.inertial_loading);
-
 	%% Torsion dist
 	% Positive torque clockwise
 	% Needs to include:
@@ -139,10 +136,11 @@
 
 	% Lift force calc
 	x_ac_percent_c = 0.25;
-	lift.torsional_load = -lift.L_dist*.(x_ac_percent_c - x_sc_assumption_percent_c)*.c(spanwise_disc);
+	lift.torsional_load = -lift.L_dist.*(x_ac_percent_c - x_sc_assumption_percent_c).*c(spanwise_disc);
 
 	% Pitching moment inclusion
-	lift.pitching_moment_load = ...;
+	%c_mac = 1.2607;
+	lift.pitching_moment_load = 0.5*rho0*Vinf_eas^2*c(spanwise_disc).^2*cm0;
 
 	% Fuel tank contribution
 	x_fuel_percent_c = 0.5; % DOES THIS ASSUMPTION MAKE SENSE
@@ -153,15 +151,15 @@
 	wing.torsional_load = -wing.inertial_loading.*(wing.centroid_x_percent_c - x_sc_assumption_percent_c).*(c(x1_arr) + c(x2_arr))/2; % dimensionalise by avg cord between x1 and x2
 
 	% UC weight contribution (update if isLanding)
-	uc.attachment_point_percent_c = ...;
+	uc.attachment_point_percent_c = (c(1.18) - 0.1)/c(1.18);
 	uc.torsional_load(x) = -uc.loading(x)*(uc.attachment_point_percent_c - x_sc_assumption_percent_c)*c(x);
 
 	if (isLanding)
 		xcg = 4.55; % measured from the nose in m
 		xG = 5.16;
 		xT = 10.3474;
-		W = 3128.2*newtons_to_lbs*0.85; % landing weight is 85% of mass takeoff weight
-		F_uc = W*g*(xcg - xT)/(xG - xT);
+		W = 3128.2*lbs_to_kg*0.85; % landing weight is 85% of mass takeoff weight
+		F_uc = W*g*(xcg - xT)/(xG - xT); % Points up!! => positive
 
 		uc.loading(x) = piecewise((x >= uc.spanwise_start) & (x <= uc.spanwise_end), F_uc, 0);
 		uc.torsional_load(x) = -uc.loading(x)*(uc.attachment_point_percent_c - x_sc_assumption_percent_c)*c(x);
@@ -170,69 +168,95 @@
 		lift.torsional_load = 0;
 
 		lift.pitching_moment_load = 0;
-	end
 
-		
+		%% Ah ffs might have to include some flap loads
+    end
+    
+    combined_loading = double(uc.loading(spanwise_disc) + fuel.loading(x1_arr, x2_arr) + lift.L_dist + wing.inertial_loading);
 	shear_dist = sum(combined_loading) - cumsum(combined_loading) + combined_loading;
 
 	temp = movsum(shear_dist, 2)*delta_s/2;
 	dM = [temp(2:end), 0];
 
 	bm_dist = sum(dM) - cumsum(dM) + dM;
+    
+    %lift.pitching_moment_load = 0;
+	torsional_dist = double(uc.torsional_load(spanwise_disc) + wing.torsional_load + lift.pitching_moment_load + lift.torsional_load); 
+	torque_dist = sum(torsional_dist) - cumsum(torsional_dist) + torsional_dist;
 
-	figure;
-	hold on;
-	plot(spanwise_disc, combined_loading);
-	xline(bottom_edge_intercept, 'r')
-	% plot(X_hatch, Y_hatch, 'r')
-	xlim([0 semispan]);
-	ylim([0 max(combined_loading)*1.1])
-	grid on;
-	title("Combined Loading")
-	hold off
+	if doPlot
+		figure;
+		subplot(5,1,1)
+		hold on;
+		plot(spanwise_disc, torsional_dist);
+		grid on;
+		title(append(loadingCaseStr, "Combined Torque"))
+		hold off;
 
-	figure;
-	hold on;
-	plot(spanwise_disc, shear_dist);
-	xline(bottom_edge_intercept, 'r')
-	grid on;
-	title("Shear Loading")
-	xlim([0 semispan]);
-	ylim([0 max(shear_dist)*1.1])
-	hold off;
+		subplot(5,1,2)
+		hold on;
+		plot(spanwise_disc, torque_dist);
+		grid on;
+		title(append(loadingCaseStr, "Torque Dist"))
+		hold off;
 
-	figure;
-	hold on;
-	plot(spanwise_disc, bm_dist);
-	xline(bottom_edge_intercept, 'r')
-	grid on;
-	title("Bending Moment")
-	xlim([0 semispan]);
-	ylim([0 max(bm_dist)*1.1])
-	hold off;
+		subplot(5,1,3)
+		hold on;
+		plot(spanwise_disc, combined_loading);
+		xline(bottom_edge_intercept, 'r')
+		% plot(X_hatch, Y_hatch, 'r')
+		xlim([0 semispan]);
+		% ylim([0 max(combined_loading)*1.1])
+		grid on;
+		title(append(loadingCaseStr, "Combined Loading"))
+		hold off
 
-	figure;
-	hold on;
-	plot(spanwise_disc, lift.L_dist);
-	xline(bottom_edge_intercept, 'r')
-	grid on;
-	title("Lift Distribution")
-	xlim([0 semispan]);
-	ylim([0 max(lift.L_dist)*1.1])
-	hold off;
+		subplot(5,1,4)
+		hold on;
+		plot(spanwise_disc, shear_dist);
+		xline(bottom_edge_intercept, 'r')
+		grid on;
+		title(append(loadingCaseStr, "Shear Loading"))
+		xlim([0 semispan]);
+		% ylim([0 max(shear_dist)*1.1])
+		hold off;
 
-	figure;
-	hold on;
-	plot(spanwise_disc, wing.inertial_loading);
-	xline(bottom_edge_intercept, 'r')
-	grid on;
-	title("Wing Weight Distribution (Debug only)")
-	xlim([0 semispan]);
-	ylim([0 max(wing.inertial_loading)*1.1])
-	hold off;
+		subplot(5,1,5);
+		hold on;
+		plot(spanwise_disc, bm_dist);
+		xline(bottom_edge_intercept, 'r')
+		grid on;
+		title(append(loadingCaseStr, "Bending Moment"))
+		xlim([0 semispan]);
+		% ylim([0 max(bm_dist)*1.1])
+		hold off;
 
-	% hold on;
-	% plot(bottom_linspace, fuel.loading(bottom_linspace) + wing.loading(bottom_linspace) + uc.loading(bottom_linspace) - fus.r);
-	% hold off;
+		figure;
+		subplot(2,1,1);
+		hold on;
+		plot(spanwise_disc, lift.L_dist);
+		xline(bottom_edge_intercept, 'r')
+		grid on;
+		title(append(loadingCaseStr, "Lift Distribution"))
+		xlim([0 semispan]);
+		% if (~isLanding)
+		%     ylim([0 max(lift.L_dist)*1.1]);
+		% end
+		hold off;
 
-% end
+		subplot(2,1,2);
+		hold on;
+		plot(spanwise_disc, wing.inertial_loading);
+		xline(bottom_edge_intercept, 'r')
+		grid on;
+		title(append(loadingCaseStr, "Wing Weight Distribution (Debug only)"))
+		xlim([0 semispan]);
+		% ylim([max(double(wing.inertial_loading))*1.1, 0])
+		hold off;
+
+		% hold on;
+		% plot(bottom_linspace, fuel.loading(bottom_linspace) + wing.loading(bottom_linspace) + uc.loading(bottom_linspace) - fus.r);
+		% hold off;
+	end
+
+end
