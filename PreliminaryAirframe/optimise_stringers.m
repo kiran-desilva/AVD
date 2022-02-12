8415c8e6dcde642465d5165ad844ad4dfc44a05e
Pavol Margitfalvi <margitpav1@gmail.com>
2022-02-12 19:48:03 +0000

    Added functions to calculate box width and height

--- PreliminaryAirframe/optimise_stringers.m
+++ PreliminaryAirframe/optimise_stringers.m
@@ -2,19 +2,89 @@ clear
 clc
 close all
 
+airfoilcoords.top = [1.00000     0.00000;
+0.95033     0.00986;
+0.90066     0.01979;
+0.85090     0.02974;
+0.80103     0.03935;
+0.75107     0.04847;
+0.70101     0.05686;
+0.65086     0.06440;
+0.60064     0.07085;
+0.55035     0.07602;
+0.50000     0.07963;
+0.44962     0.08139;
+0.39923     0.08139;
+0.34884     0.07971;
+0.29846     0.07658;
+0.24881     0.07193;
+0.19781     0.06562;
+0.14757     0.05741;
+0.09746     0.04672;
+0.07247     0.04010;
+0.04757     0.03227;
+0.02283     0.02234;
+0.01059     0.01588;
+0.00580     0.01236;
+0.00347     0.01010;
+0.00000     0.00000];
+
+airfoilcoords.bottom = [ 0 0;
+0.00653     -0.00810;
+0.00920     -0.00956;
+0.01441     -0.01160;
+0.02717     -0.01490;
+0.05243     -0.01963;
+0.07753     -0.02314;
+0.10254     -0.02604;
+0.15243     -0.03049;
+0.20219     -0.03378;
+0.25189     -0.03613;
+0.30154     -0.03770;
+0.35116     -0.03851;
+0.40077     -0.03855;
+0.45038     -0.03759;
+0.50000     -0.03551;
+0.54965     -0.03222;
+0.59936     -0.02801;
+0.64914     -0.02320;
+0.69899     -0.01798;
+0.74893     -0.01267;
+0.79897     -0.00751;
+0.84910     -0.00282;
+0.89934     0.00089;
+0.94967     0.00278;
+1.00000     0.00000];
+
 load('limiting_loadcase_distributions');
 c_root = 1.767;
 c_tip = 0.533;
 
-syms y;
+top_af_fit = fit(airfoilcoords.top(:, 1), airfoilcoords.top(:, 2), 'smoothingspline');
+bottom_af_fit = fit(airfoilcoords.bottom(:, 1), airfoilcoords.bottom(:, 2), 'smoothingspline');
+
+x = linspace(0, 1, 100);
+figure
+hold on;
+plot(x, top_af_fit(x), 'k');
+plot(x, bottom_af_fit(x), 'k');
+axis equal;
+grid on;
+hold off;
+
 geometry.semispan = 8.94/2;
-geometry.c(y) = -(c_root - c_tip)/geometry.semispan*y + c_root;
-geometry.box_width_func(y) = 0*y;
-geometry.web_height_func(y) = 0*y;
 geometry.sweep_deg = 18.17;
 geometry.spar.front_x_c = 0.1;
 geometry.spar.rear_x_c = 0.77;
 geometry.airfoil = NaN; %% TODO:
+
+geometry.c = @(y) -(c_root - c_tip)/geometry.semispan*y + c_root;
+
+geometry.box_width_func = @(y) abs(geometry.spar.front_x_c - geometry.spar.rear_x_c)*c(y);
+front_web_height = abs(top_af_fit(geometry.spar.front_x_c) - bottom_af_fit(geometry.spar.front_x_c));
+rear_web_height = abs(top_af_fit(geometry.spar.rear_x_c) - bottom_af_fit(geometry.spar.rear_x_c));
+geometry.web_height_func = @(y) max(front_web_height, rear_web_height)*c(y); %% TODO: Play with the max function, try min or avg instead
+																			 %% 	  max should however give largest stress
 
 material.E = 70E9;
 material.poisson_r = 1/3;
