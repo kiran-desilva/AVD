870a0392652d508fdfb596629dfe1c84afbd0b6d
kiran <kiran@kirans-mbp.home>
2022-02-13 12:55:15 +0000

    update

--- PreliminaryAirframe/materialLibGenerator.m
+++ PreliminaryAirframe/materialLibGenerator.m
@@ -3,7 +3,7 @@ clc
 
 %SI UNITS
 
-materialLib{1}.name = 'Al example';
+materialLib{1}.name = 'Al 2024';
 materialLib{1}.E = 73000; %N/mm^2
 materialLib{1}.v = 0.33;
 materialLib{1}.G = 27444; %N/mm^2
