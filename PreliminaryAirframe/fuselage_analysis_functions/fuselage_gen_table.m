0da2fb1c971cfbd2a4f40352fa36fc347f14dc7f
kiran <kd619@ic.ac.uk>
2022-02-27 00:44:18 +0000

    tablez

--- PreliminaryAirframe/fuselage_analysis_functions/fuselage_gen_table.m
+++ PreliminaryAirframe/fuselage_analysis_functions/fuselage_gen_table.m
@@ -1,17 +1,63 @@
-function [] = fuselage_gen_table(fuselage)
-    StringerPanel.Labels = {'Weight [N]';
-                            'Fuselage Skin Thickness [m]';
+function [StringerPanel] = fuselage_gen_table(fuselage)
+    StringerPanel.Labels = {'Weight';
+                            'Fuselage Skin Thickness';
                             'Number of Stringers';
-                            'Stringer Thickness [m]';
-                            'Stringer Web Height [m]';
+                            'Stringer Section Type';
+                            'Stringer Thickness';
+                            'Stringer Web Height';
                             'Stringer Web to Flange Ratio'};
-    StringerPanel.Data = [fuselage.stringerpanel.weight;
-                        fuselage.stringerpanel.booms.skin_thickness;
+    StringerPanel.Units = {
+        'N';
+        'mm';
+        '-';
+        '-';
+        'mm';
+        'mm';
+        '-';
+    };
+    StringerPanel.Data = {fuselage.stringerpanel.weight;
+                        fuselage.stringerpanel.booms.skin_thickness*(1e3);
                         length(fuselage.stringerpanel.booms.phi);
-                        fuselage.stringer.thickness;
-                        fuselage.stringer.web_height;
-                        fuselage.stringer.flange_to_web_ratio];
+                        'Z';
+                        fuselage.stringer.thickness*(1e3);
+                        fuselage.stringer.web_height*(1e3);
+                        fuselage.stringer.flange_to_web_ratio};
     
-    StringerPanel.table = table(StringerPanel.Data)
+    StringerPanel.table = table(StringerPanel.Units,StringerPanel.Data)
     StringerPanel.table.Properties.RowNames = StringerPanel.Labels;
+    StringerPanel.table.Properties.VariableNames = {'Units','Value'};
     table2latex(StringerPanel.table,'Tables/fuselage_stringerpanel_table.tex');
+
+
+    LightFrames.Labels = {'Weight';
+                        'Spacing';
+                        'Number of Frames';
+                        'Frame Section Type'
+                        'Frame Thickness';
+                        'Frame Height';
+                        'Frame Width'};
+
+    LightFrames.Units = {
+        'N';
+        'm';
+        '-';
+        '-';
+        'mm';
+        'mm';
+        'mm';
+    };
+    LightFrames.Data = {
+        fuselage.frames.weight;
+        fuselage.frames.L;
+        fuselage.frames.number;
+        'C';
+        fuselage.frames.t;
+        fuselage.frames.h;
+        fuselage.frames.b;
+
+    };
+    
+    LightFrames.table = table(LightFrames.Units,LightFrames.Data)
+    LightFrames.table.Properties.RowNames = LightFrames.Labels;
+    LightFrames.table.Properties.VariableNames = {'Units','Value'};
+    table2latex(LightFrames.table,'Tables/fuselage_lightframes_table.tex');
