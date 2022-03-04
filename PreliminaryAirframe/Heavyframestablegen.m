f0786a3946377285b74b847f4aeba83b0851aeb1
cking <chorleyblueking@gmail.com>
2022-03-04 10:27:56 +0000

    lwknlkew

--- PreliminaryAirframe/Heavyframestablegen.m
+++ PreliminaryAirframe/Heavyframestablegen.m
@@ -6,10 +6,10 @@ fig_path = fullfile('./Tables');
 load('HeavyFrames.mat');
 heavyframes.Labels = {'Heavy Frame','Mass (Kg)','Web Thickness (m)','Flange Thickness','Web Height (m)','Flange Length (m)'};
 
-framenames = {'Wing Front Spar'; 'Wing Rear Spar'; 'Tail Front Spar'; 'Tail Rear Spar'};
-masses = [HeavyFrames.wing.massfront; HeavyFrames.wing.massrear; HeavyFrames.engine.mass; HeavyFrames.tail.mass];
+framenames = {'Wing Front'; 'Wing Rear'; 'Tail Front'; 'Tail Rear'};
+masses = [round(HeavyFrames.wing.massfront, 1); round(HeavyFrames.wing.massrear,1); round(HeavyFrames.engine.mass,1); round(HeavyFrames.tail.mass,1)];
 tweb = [HeavyFrames.wing.twFRONT; HeavyFrames.wing.twREAR; HeavyFrames.engine.t; HeavyFrames.tail.tvREAR];
-lflange = [HeavyFrames.wing.lfwFRONT; HeavyFrames.wing.lfwREAR; HeavyFrames.engine.lf; HeavyFrames.tail.lfvREAR];
+lflange = [round(HeavyFrames.wing.lfwFRONT,3); round(HeavyFrames.wing.lfwREAR,3); round(HeavyFrames.engine.lf,3); round(HeavyFrames.tail.lfvREAR,3)];
 hweb = [HeavyFrames.wing.Hfront; HeavyFrames.wing.Hrear; HeavyFrames.engine.H; HeavyFrames.tail.H];
 %tweb = [HeavyFrames.wing.twFRONT; HeavyFrames.wing.twREAR; HeavyFrames.engine.t; HeavyFrames.tail.tvREAR];
 tflange = [HeavyFrames.wing.tffront; HeavyFrames.wing.tfrear; HeavyFrames.engine.tf; HeavyFrames.tail.tf];
